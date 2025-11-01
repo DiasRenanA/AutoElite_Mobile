ca6a0579f0e892eab22a016daa05a7ec166f8616
Renan Dias <diasabrantesrenan@gmail.com>
2025-11-01 17:32:11 -0300

    icon salve

--- src/components/inputComponent/index.tsx
+++ src/components/inputComponent/index.tsx
@@ -1,14 +1,21 @@
-import { TextInput, View } from "react-native"
+import { Image, TextInput, View } from "react-native"
 import { Styles } from "./style"
 
 export const Input = () => {
     return(
         <View>
-            <TextInput
-                style={Styles.textInput}
-                placeholder="Pesquisar..."
-                placeholderTextColor="#fff"
-            />
+            <View style={Styles.BoxInput}>
+                <TextInput
+                    style={Styles.textInput}
+                    placeholder="Pesquisar..."
+                    placeholderTextColor="#fff"
+                    underlineColorAndroid="transparent"
+                />
+                <Image
+                    source={require('@/src/assets/images/lupa_icon.svg')}
+                    resizeMode="contain"
+                />
+            </View>
         </View>
     )
 }
--- src/components/inputComponent/style.ts
+++ src/components/inputComponent/style.ts
@@ -1,7 +1,21 @@
 import { StyleSheet } from "react-native";
 
 export const Styles = StyleSheet.create({
+    BoxInput:{
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: '#000',
+        width: '100%',
+        alignItems: 'center',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        
+    },
+
     textInput:{
-        backgroundColor: '#000'
+        color: '#fff',
+        padding: 5,
+        flex: 1,
+        borderWidth: 0
     }
 })
